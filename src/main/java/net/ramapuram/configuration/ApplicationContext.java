c4f84523d9d5b8e510e9af21480a865f49994bdc
ramapuramgeetha <ramapuram.geetha@gmail.com>
2016-09-28 21:19:04 +0530

    First Commit, no error but tables not created

---
+++ src/main/java/net/ramapuram/configuration/ApplicationContext.java
@@ -0,0 +1,78 @@
+package net.ramapuram.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import net.ramapuram.dao.User;
+//import net.ramapuram.model.UserDAO;
+//import net.ramapuram.model.UserDAOImpl;
+
+	@Configuration
+	@ComponentScan("net.ramapuram.dao")
+	@EnableTransactionManagement
+
+	public class ApplicationContext {
+
+		// The dataSource Bean
+		@Bean(name = "dataSource")
+		public DataSource   getDataSource() {
+		    BasicDataSource dataSource = new BasicDataSource();
+		    dataSource.setDriverClassName("org.h2.Driver");
+		    dataSource.setUrl("jdbc:h2:tcp://localhost/~/test1");
+		    dataSource.setUsername("sa");
+		    dataSource.setPassword("");
+		 
+		    return dataSource;
+		}
+		
+		private Properties getHibernateProperties() {
+		    Properties properties = new Properties();
+		    properties.put("hibernate.show_sql", "true");
+		    properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		    properties.put("hibernate.hbm2ddl.auto","update");
+		    properties.put("hibernate.format_sql","true");
+		    return properties;
+		}
+		
+		@Autowired
+		@Bean(name = "sessionFactory")
+		public SessionFactory getSessionFactory(DataSource dataSource) {
+		 
+		    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		 
+		    sessionBuilder.addAnnotatedClasses(User.class);
+		    sessionBuilder.scanPackages("net.ramapuram.model");
+		 
+		    return sessionBuilder.buildSessionFactory();
+		}
+		
+		@Autowired
+		@Bean(name = "transactionManager")
+		public HibernateTransactionManager getTransactionManager(
+		        SessionFactory sessionFactory) {
+		    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+		            sessionFactory);
+		 
+		    return transactionManager;
+		}
+		
+		/*
+		@Autowired
+		@Bean(name = "userDao")
+		public UserDAO getUserDao(SessionFactory sessionFactory) {
+		    return new UserDAOImpl(sessionFactory);
+		}
+*/
+
+}
---
+++ src/main/java/net/ramapuram/dao/User.java
@@ -0,0 +1,54 @@
+package net.ramapuram.dao;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.Generated;
+
+@Entity (name="USER")
+public class User {
+ 
+    @Id
+	private int id;
+    private String username;
+    private String password;
+    private String email;
+    
+	public User() {
+
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
+
